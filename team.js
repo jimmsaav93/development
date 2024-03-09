2dc0d173804c690f2ce37e99de0dd2bd375505b4
Jimmy Saavedra <72888568+jimmsaav@users.noreply.github.com>
2024-03-09 05:36:27 -0500

    Dati

---
+++ team.js
@@ -0,0 +1,14 @@
+const dati = [
+    {
+    nome : "Jimmy",
+    cognome : "Saavedra",
+    età : 30,
+    città : "Genova",
+    hobby : "Trading",
+    ciboPreferito : "Encebollado",
+    videogiocoPreferito : "LOL",
+    filmPreferito : "Resident Evil",
+    libroPreferito : "Asesino Accidental",
+    nomeAnimaleDomestico : Saoko
+    }
+];
