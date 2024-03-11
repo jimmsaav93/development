29a08da18c989505bc9c5236606582f4fbcec537
Francesca3003 <francescapischedda30@gmail.com>
2024-03-11 10:17:54 +0100

    add info Francesca

---
+++ infoF.js
@@ -0,0 +1,14 @@
+const info = [
+  {
+    nome: "Francesca",
+    cognome: "Pischedda",
+    età: 32,
+    città: "Cagliari",
+    hobby: "Cucinare",
+    ciboPreferito: "Pizza",
+    videogiocoPreferito: " ",
+    filmPreferito: "Pretty Woman",
+    libroPreferito: "Succede sempre qualcosa di meraviglioso",
+    nomeAnimaleDomestico: "Peggy",
+  },
+];
