5af34cd9ff09bee2fd0c79c36a7ee8b4663c9720
David <davidsofi01@gmail.com>
2024-03-11 10:55:44 +0100

    addinfo David

---
+++ InfoD.js
@@ -0,0 +1,15 @@
+const info = [
+    {
+      nome: "David",
+      cognome: "Sofia",
+      età: 22,
+      città: "Palermo",
+      hobby: "Gaming",
+      ciboPreferito: "Pizza",
+      videogiocoPreferito: " Pokèmon nero 2",
+      filmPreferito: "Shrek",
+      libroPreferito: "Accademia del bene e del male",
+      nomeAnimaleDomestico: "",
+    },
+  ];
+  
