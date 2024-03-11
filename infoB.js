9beb4b6d5fb4fc0180f851790f8bd8e9919bce59
bryan gonzalez <ikemutante@gmail.com>
2024-03-11 11:13:49 +0100

    brayan info

---
+++ infoB.js
@@ -0,0 +1,15 @@
+const info = [
+    {
+      nome: "Brayan",
+      cognome: "gonzalez",
+      età: 28,
+      città: "Roma",
+      hobby: "read",
+      ciboPreferito: "Pasta",
+      videogiocoPreferito: " bloddy roard",
+      filmPreferito: "resident evil",
+      libroPreferito: "la tradizione occulta",
+      nomeAnimaleDomestico: "Rafaelo",
+    },
+  ];
+  
