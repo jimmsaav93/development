0659cc607ea3cecd994d22e2eec5df9da5762fa8
Francesca3003 <francescapischedda30@gmail.com>
2024-03-11 09:57:34 +0100

    add info Francesca

--- team.js
+++ team.js
@@ -1,14 +1,26 @@
 const dati = [
-    {
-    nome : "Jimmy",
-    cognome : "Saavedra",
-    età : 30,
-    città : "Genova",
-    hobby : "Trading",
-    ciboPreferito : "Encebollado",
-    videogiocoPreferito : "LOL",
-    filmPreferito : "Resident Evil",
-    libroPreferito : "Asesino Accidental",
-    nomeAnimaleDomestico : Saoko
-    }
+  {
+    nome: "Jimmy",
+    cognome: "Saavedra",
+    età: 30,
+    città: "Genova",
+    hobby: "Trading",
+    ciboPreferito: "Encebollado",
+    videogiocoPreferito: "LOL",
+    filmPreferito: "Resident Evil",
+    libroPreferito: "Asesino Accidental",
+    nomeAnimaleDomestico: "Saoko",
+  },
+  {
+    nome: "Francesca",
+    cognome: "Pischedda",
+    età: 32,
+    città: "Cagliari",
+    hobby: "Cucinare",
+    ciboPreferito: "Pizza",
+    videogiocoPreferito: " ",
+    filmPreferito: "Pretty Woman",
+    libroPreferito: "Succede sempre qualcosa di meraviglioso",
+    nomeAnimaleDomestico: "Peggy",
+  },
 ];
